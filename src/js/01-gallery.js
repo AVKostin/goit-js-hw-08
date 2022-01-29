ebc5fb580776fc422f6bf6ea55058b7f7ec68d10
AVKostin <alexander.kostin.ua@gmail.com>
2022-01-29 16:02:34 +0200

    Image gallery relise

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const galleryRef = document.querySelector('.gallery');
+const itemGalleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', itemGalleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+			<a class="gallery__link" href="${original}">
+				<img
+                    class="gallery__image"
+                        src="${preview}"
+                        alt="${description}"
+				/>
+			</a>`;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+// lightbox.on('show.simplelightbox', function () {});
