267b34e0cf1341103978fb6060f9a37a2f9aae65
AVKostin <alexander.kostin.ua@gmail.com>
2022-01-29 17:11:52 +0200

    Video Player Relise

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+const handlerCurrentTime = data => {
+  localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+};
+
+player.on('timeupdate', Throttle(handlerCurrentTime, 1000));
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
