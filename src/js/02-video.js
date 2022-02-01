be6444e6dd7b07fb9f7cd7c77e54a20e353c2aee
AVKostin <alexander.kostin.ua@gmail.com>
2022-02-01 15:13:50 +0200

    optimize code & clear mistakes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,36 @@
-import Throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-const currentTime = localStorage.getItem('videoplayer-current-time');
+const iFrame = document.querySelector('#vimeo-player');
+const VIDEO_PLAYER_CURRENT_TIME = 'videoplayer-current-time';
+const player = new Vimeo.Player(iFrame);
+const currentTime = localStorage.getItem(VIDEO_PLAYER_CURRENT_TIME);
 
 const handlerCurrentTime = data => {
-  localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+	localStorage.setItem(VIDEO_PLAYER_CURRENT_TIME, data.seconds);
 };
 
-player.on('timeupdate', Throttle(handlerCurrentTime, 1000));
+player.on('timeupdate', throttle(handlerCurrentTime, 1000));
 
 player
-  .setCurrentTime(currentTime)
-  .then(function (seconds) {})
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        break;
-      default:
-        break;
-    }
-  });
+	.setCurrentTime(currentTime)
+	.then()
+	.catch(error => {
+		switch (error.name) {
+			case 'RangeError':
+				break;
+			default:
+				break;
+		}
+	});
 
 player
-  .setVolume(0.02)
-  .then(function (volume) {})
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        break;
-      default:
-        break;
-    }
-  });
+	.setVolume(0.02)
+	.then()
+	.catch(error => {
+		switch (error.name) {
+			case 'RangeError':
+				break;
+			default:
+				break;
+		}
+	});
