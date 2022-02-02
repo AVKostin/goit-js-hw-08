0d04fdbf722b0e1bf5d481a28fbec957935ec00a
AVKostin <alexander.kostin.ua@gmail.com>
2022-02-03 00:29:32 +0200

    resolve task. work good! :)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,40 @@
 import throttle from 'lodash.throttle';
-import '../css/common.css';
-import '../css/03-feedback.css';
 const formRef = document.querySelector('.feedback-form');
-const emailRef = formRef.elements.email;
-const messageRef = formRef.elements.message;
+const email = formRef.email;
+const message = formRef.message;
 const STORAGE_KEY = 'feedback-form-state';
-let formData = {};
 updateOutput();
 
 function updateOutput() {
-	try {
-		const parseStorageKey = JSON.parse(localStorage.getItem(STORAGE_KEY));
-		emailRef.value = `${parseStorageKey.email}`;
-		messageRef.value = `${parseStorageKey.message}`;
-	} catch (error) {}
+	const parseStorageKey = JSON.parse(localStorage.getItem(STORAGE_KEY));
+	if (!parseStorageKey) {
+		return;
+	}
+	if (parseStorageKey.email || parseStorageKey.message)
+		(formRef.email.value = parseStorageKey.email),
+			(formRef.message.value = parseStorageKey.message);
 }
 
-const checkInputValue = () => {
-	if (emailRef.value !== '' || messageRef.value !== '') {
-		(formData.email = emailRef.value), (formData.message = messageRef.value);
-	}
+const onTextInput = () => {
+	const formData = {
+		email: email.value,
+		message: message.value,
+	};
 	localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
-const valueInput = e => {
+const onFormSubmit = e => {
 	e.preventDefault();
 	if (formRef.email.value === '' || formRef.message.value === '') {
 		return alert('Внимательно заполните все поля!');
 	}
-	console.log(formData);
+	const formDataInput = {
+		email: e.currentTarget.email.value,
+		massage: e.currentTarget.message.value,
+	};
+	console.log(formDataInput);
 	e.target.reset();
 	localStorage.removeItem(STORAGE_KEY);
 };
-emailRef.addEventListener('input', throttle(checkInputValue, 500));
-formRef.addEventListener('input', throttle(checkInputValue, 500));
-formRef.addEventListener('submit', valueInput);
+formRef.addEventListener('input', throttle(onTextInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
