7a5a2dec105851cc2f4a8b5f05a1aaf0f3c9c18a
AVKostin <alexander.kostin.ua@gmail.com>
2022-02-05 14:23:06 +0200

    correct some mistakes & add checkup

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,18 @@
-import { Player } from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iFrame = document.querySelector('#vimeo-player');
+const iFrameEl = document.querySelector('#vimeo-player');
 const VIDEO_PLAYER_CURRENT_TIME = 'videoplayer-current-time';
-const player = new Vimeo.Player(iFrame);
+const player = new Player(iFrameEl);
+
 const currentTime = localStorage.getItem(VIDEO_PLAYER_CURRENT_TIME);
 
 const handlerCurrentTime = data => {
 	localStorage.setItem(VIDEO_PLAYER_CURRENT_TIME, data.seconds);
 };
-player.on('timeupdate', throttle(handlerCurrentTime, 1000));
-
+if (currentTime === null) {
+	player.setCurrentTime(0);
+}
 player.setCurrentTime(currentTime);
+player.on('timeupdate', throttle(handlerCurrentTime, 1000));
 player.setVolume(0.02);
