ea3a751cf2f55dc018175890cd9cea74a359c77d
AVKostin <alexander.kostin.ua@gmail.com>
2022-02-07 16:25:58 +0200

    restored early revision code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,38 @@
 import throttle from 'lodash.throttle';
-const form = document.querySelector('.feedback-form');
-const STORAGE_KEY = 'feedback-from-state';
-const formData = {};
+const formRef = document.querySelector('.feedback-form');
+const email = formRef.email;
+const message = formRef.message;
 
-const parseStorageKey = JSON.parse(localStorage.getItem(STORAGE_KEY));
-if (parseStorageKey) {
-	form.email.value = parseStorageKey.email;
-	form.message.value = parseStorageKey.message;
+const STORAGE_KEY = 'feedback-form-state';
+updateOutput();
+let formData = {
+	email: email.value,
+	message: message.value,
+};
+
+function updateOutput() {
+	const savedInStorageKey = JSON.parse(localStorage.getItem(STORAGE_KEY));
+	if (savedInStorageKey) {
+		email.value = savedInStorageKey.email || '';
+		message.value = savedInStorageKey.message || '';
+	}
 }
+
+const onTextInput = e => {
+	formData[e.target.name] = e.target.value;
+	localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
 
 const onFormSubmit = e => {
 	e.preventDefault();
-
-	if (!form.email.value || !form.message.value) {
-		return alert('Все поля должны быть заполнены!');
+	if (!email.value || !message.value) {
+		return alert('Внимательно заполните все поля!');
 	}
-	formData[e.target.name] = e.target.value;
-	console.log({
-		email: form.email.value,
-		message: form.message.value,
-	});
-
+	console.log(formData);
+	e.currentTarget.reset();
 	localStorage.removeItem(STORAGE_KEY);
-	e.target.reset();
+	formData = {};
 };
 
-const onTextInput = () => {
-	localStorage.setItem(
-		STORAGE_KEY,
-		JSON.stringify({
-			email: form.email.value,
-			message: form.message.value,
-		}),
-	);
-};
-form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onTextInput, 500));
+formRef.addEventListener('input', throttle(onTextInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
